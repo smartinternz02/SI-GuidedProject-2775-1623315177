9a994d03fb1f3c2395214daa92badb297055140e
deepthimallula <84567392+deepthimallula@users.noreply.github.com>
2021-07-03 13:54:23 +0530

    Add files via upload
    https://github.com/smartinternz02/SI-GuidedProject-2775-1623315177/upload
    Node Red : https://node-red-vfvle-2021-05-12.eu-gb.mybluemix.net/red/#flow/c698b36.f00415

---
+++ hazard.py
@@ -0,0 +1,30 @@
+import wiotp.sdk.device
+import time
+import random
+myConfig = { 
+    "identity": {
+        "orgId": "2wqkug",
+        "typeId": "iotdevice",
+        "deviceId":"1001"
+    },
+    "auth": {
+        "token": "1234567890"
+    }
+}
+
+def myCommandCallback(cmd):
+    print("Message received from IBM IoT Platform: %s" % cmd.data['command'])
+    m=cmd.data['command']
+
+client = wiotp.sdk.device.DeviceClient(config=myConfig, logHandlers=None)
+client.connect()
+
+while True:
+    temp=random.randint(-20,125)
+    hum=random.randint(0,100)
+    myData={'temperature':temp, 'humidity':hum}
+    client.publishEvent(eventId="status", msgFormat="json", data=myData, qos=0, onPublish=None)
+    print("Published data Successfully: %s", myData)
+    client.commandCallback = myCommandCallback
+    time.sleep(2)
+client.disconnect()
